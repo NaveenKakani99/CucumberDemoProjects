4cfce9ba40c309edc00d28e745c199098fc78375
naveen.kakani <naveen.kakani@DGSLBLRDT186.mshome.net>
2020-03-12 09:58:42 +0530

    My first Commit

---
+++ CucumberAutomationDemo/src/test/java/Steps/LoginSteps.java
@@ -0,0 +1,20 @@
+package Steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+	
+	@Given("^User is already on login page$")
+	public void UserIsAlreadyOnLoginPage()
+	{
+		
+	}
+	
+	@When("")
+	public void My()
+	{
+	
+	}
+
+}
---
+++ CucumberAutomationDemo/src/test/java/futures/Login.feature
@@ -0,0 +1,6 @@
+Feature: Free CRM Login feature
+
+Scenario: free CRM Login Test scenario
+Given User is already on login page
+When user enter 'username' and 'password'
+Then user should login successfully 
